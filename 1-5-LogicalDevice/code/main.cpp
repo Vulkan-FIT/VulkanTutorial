1ab751147f6a8f7f4d29f50c61211702ac6dfb56
PCJohn <peciva@fit.vut.cz>
2025-02-09 13:01:52 +0100

    Added 1-5-LogicalDevice example code.

---
+++ 1-5-LogicalDevice/code/main.cpp
@@ -0,0 +1,140 @@
+#include <array>
+#include <iostream>
+#ifdef _WIN32
+# define WIN32_LEAN_AND_MEAN
+# include <windows.h>
+//# include <codecvt>
+//# include <locale>
+#else
+# include <dlfcn.h>
+#endif
+#include "vkg.h"
+
+using namespace std;
+
+
+// return name of library that contains the specified address (address of function, symbol, etc.)
+string getLibraryOfAddr(void* addr)
+{
+	if(addr == nullptr)
+		throw std::runtime_error("Function getLibraryOfAddr() cannot resolve nullptr.");
+
+#ifdef _WIN32
+
+	// get module handle of belonging to address
+	HMODULE handle;
+	BOOL b = GetModuleHandleExA(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+	                            reinterpret_cast<LPCSTR>(addr), &handle);
+	if(b==0 || handle==nullptr)
+		throw std::runtime_error("Function GetModuleHandleExA() failed.");
+
+	// get module path
+	WCHAR wpath[MAX_PATH];
+	DWORD r = GetModuleFileNameW(handle, wpath, MAX_PATH);
+	if(r==0 || r==MAX_PATH)
+		throw std::runtime_error("Function GetModuleFileNameW() failed.");
+
+	// convert to utf-8
+	char path[MAX_PATH];
+	int l = WideCharToMultiByte(CP_UTF8, 0, wpath, -1, path, MAX_PATH, NULL, NULL);
+	if(l==0)
+		throw std::runtime_error("Function WideCharToMultiByte() failed.");
+
+	// return string
+	return string(path, l);
+
+#else
+	Dl_info dlInfo;
+	dladdr(addr, &dlInfo);
+	return string(dlInfo.dli_fname);
+#endif
+}
+
+
+int main(int argc, char* argv[])
+{
+	// catch exceptions
+	// (vk functions throw if they fail)
+	try {
+
+		// load Vulkan library
+		vk::loadLib();
+
+		// Vulkan instance
+		bool vulkan10enforced;
+		vk::initInstance(
+			vk::InstanceCreateInfo{
+				.sType = vk::StructureType::eInstanceCreateInfo,
+				.pNext = nullptr,
+				.flags = 0,
+				.pApplicationInfo =
+					&(const vk::ApplicationInfo&)vk::ApplicationInfo{
+						.sType = vk::StructureType::eApplicationInfo,
+						.pNext = nullptr,
+						.pApplicationName = "1-5-LogicalDevice",
+						.applicationVersion = 0,
+						.pEngineName = nullptr,
+						.engineVersion = 0,
+						.apiVersion = vk::ApiVersion10,
+					},
+				.enabledLayerCount = 0,
+				.ppEnabledLayerNames = nullptr,
+				.enabledExtensionCount = 0,
+				.ppEnabledExtensionNames = nullptr,
+			}
+		);
+
+		// instance function pointers
+		cout << "Instance function pointers:" << endl;
+		cout << "   vkCreateInstance()     points to: " << getLibraryOfAddr(vk::getGlobalProcAddr<void*>("vkCreateInstance")) << endl;
+		cout << "   vkCreateShaderModule() points to: " << getLibraryOfAddr(vk::getInstanceProcAddr<void*>("vkCreateShaderModule")) << endl;
+		cout << "   vkQueueSubmit()        points to: " << getLibraryOfAddr(vk::getInstanceProcAddr<void*>("vkQueueSubmit")) << endl;
+
+		// print device list
+		vk::Vector<vk::PhysicalDevice> deviceList = vk::enumeratePhysicalDevices();
+		for(size_t i=0; i<deviceList.size(); i++) {
+
+			// get device properties
+			vk::PhysicalDevice pd = deviceList[i];
+			vk::PhysicalDeviceProperties properties = vk::getPhysicalDeviceProperties(pd);
+
+			// create device
+			vk::initDevice(
+				pd,  // physicalDevice
+				vk::DeviceCreateInfo{  // pCreateInfo
+					.flags = 0,
+					.queueCreateInfoCount = 1,  // at least one queue is mandatory
+					.pQueueCreateInfos = array<vk::DeviceQueueCreateInfo,1>{
+						vk::DeviceQueueCreateInfo{
+							.flags = 0,
+							.queueFamilyIndex = 0,
+							.queueCount = 1,
+							.pQueuePriorities = &(const float&)1.f,
+						}
+					}.data(),
+					.enabledLayerCount = 0,  // no enabled layers
+					.ppEnabledLayerNames = nullptr,
+					.enabledExtensionCount = 0,  // no enabled extensions
+					.ppEnabledExtensionNames = nullptr,
+					.pEnabledFeatures = nullptr,  // no enabled features
+				}
+			);
+
+			// device function pointers
+			cout << "Device function pointers for " << properties.deviceName << ":" << endl;
+			cout << "   vkCreateShaderModule() points to: " << getLibraryOfAddr(vk::getDeviceProcAddr<void*>("vkCreateShaderModule")) << endl;
+			cout << "   vkQueueSubmit()        points to: " << getLibraryOfAddr(vk::getDeviceProcAddr<void*>("vkQueueSubmit")) << endl;
+
+		}
+
+	// catch exceptions
+	} catch(vk::Error& e) {
+		cout << e.what() << endl;
+	} catch(exception& e) {
+		cout << "Failed because of exception: " << e.what() << endl;
+	} catch(...) {
+		cout << "Failed because of unspecified exception." << endl;
+	}
+
+	return 0;
+}
